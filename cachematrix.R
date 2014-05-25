b11ea83a089f7126ebbfa28e51229da091224384
asgerkring <git@kring.dk>
2014-05-26 01:32:32 +0200

    cachematrix functions
    This file contains the two functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,26 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function makes a cached version of a matrix. A matrix will
+## have to be provided for input.
 makeCacheMatrix <- function(x = matrix()) {
 
+        invMat <- NULL  ## Reset  invMat
+        ## These 4 functions are similar to the one in the example
+        ## provided in the assignment instructions.
+        set <- function(y) {
+                x <<- y
+                invMat <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(invMatrix) invMat <<- invMatrix
+        getinverse <- function() invMat
+        
+        ## In the last part we basically "name" the various methods
+        ## makeCacheMatrix contains.
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +29,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        a <- x$getinverse()
+        ## Test if the solution is cached. If so, return it.
+        if (!is.null(a)){
+                print ("Retrieving cached matrix....")
+                return(a)
+        }
+        ## If we get to this point, the inverse matrix is not cached
+        ## Hence we have to solve it, and store the solution.
+        data <- x$get()
+        a <- solve(data, ...)
+        x$setinverse(a)
+        a
 }
